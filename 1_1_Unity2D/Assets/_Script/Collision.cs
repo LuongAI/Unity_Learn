ff5cc2840a3fc2b3476f0659e159bef69a9c69e1
LuongAI <154398943+LuongAI@users.noreply.github.com>
2026-02-14 13:21:09 +0700

    14/02

---
+++ 1_1_Unity2D/Assets/_Script/Collision.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collision : MonoBehaviour
+{
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("Ối Dời Ơi! Tôi đã đâm vào: " + collision.gameObject.name);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("Cái éo gì vậy ?: " + collision.gameObject.name);
+    }
+}
---
+++ 1_1_Unity2D/Assets/_Script/Driver.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Driver : MonoBehaviour
+{
+    [SerializeField] float steerSpeed = 120f;
+    [SerializeField] float moveSpeed = 2f;
+    [SerializeField] float boostMultiplier = 2f;
+    float moveAmount;
+    float steerAmount;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Let's control your car");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        int move = 0;
+        int steer = 0;
+        bool isBoosting = Keyboard.current.leftShiftKey.isPressed
+               || Keyboard.current.rightShiftKey.isPressed;
+
+        if (Keyboard.current.wKey.isPressed)
+        {
+            move = 1;
+        }
+        if (Keyboard.current.aKey.isPressed)
+        {
+            steer = 1;
+        }
+        if (Keyboard.current.sKey.isPressed)
+        {
+            move = -1;
+        }
+        if (Keyboard.current.dKey.isPressed)
+        {
+            steer = -1;
+        }
+        float currentMoveSpeed = isBoosting ? moveSpeed * boostMultiplier : moveSpeed;
+        moveAmount = move * currentMoveSpeed * Time.deltaTime;
+        steerAmount = steer * steerSpeed * Time.deltaTime;
+        transform.Translate(0, moveAmount, 0);
+        transform.Rotate(0, 0, steerAmount);
+
+    }
+}
